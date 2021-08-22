6cafb5d00456d6901e7a096fa693d5986ff5698b
Roger <rogerx123@gmail.com>
2021-08-22 12:27:30 -0700

    Format updates

--- sql/196_Delete_Duplicate_Emails.sql
+++ sql/196_Delete_Duplicate_Emails.sql
@@ -1,9 +1,9 @@
-// 196. Delete Duplicate Emails
-//   Keep only unique emails based on its smallest Id.
+-- 196. Delete Duplicate Emails
+--   Keep only unique emails based on its smallest Id.
 
 DELETE FROM Person
 WHERE Id NOT IN
-    (SELECT a.minId FROM
-            (SELECT MIN(Id) AS minId FROM Person GROUP BY Email)
-	         AS a);
+  (SELECT a.minId FROM
+     (SELECT MIN(Id) AS minId FROM Person GROUP BY Email) as a
+  );
 		 
